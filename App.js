792e24f563858133293dc3783f320754a334de67
VolhaKaldziankova <63316949+VolhaKaldziankova@users.noreply.github.com>
2020-06-21 09:42:54 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Store from './components/Store';
+
+let booksArr=require('./books.json');
+let nameStore='Магазин книг'
+
+ReactDOM.render( 
+  <Store books={booksArr} nameStore={nameStore}/>
+, document.getElementById('container') );
