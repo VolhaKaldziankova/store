a19cb05ebf396dc637cbd8e4885be53e424839b1
VolhaKaldziankova <63316949+VolhaKaldziankova@users.noreply.github.com>
2020-07-05 21:12:03 +0300

    Update Store.js

--- components/Store.js
+++ components/Store.js
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Store.css';
@@ -11,9 +11,22 @@ class Store extends React.PureComponent {
     books: PropTypes.array.isRequired
   };
 
+  state ={
+    viewPage: 1 //вид страницы: 1 - в виде списка, 2 - в виде таблицы
+  }
+
+  ChangeList = () => {
+    this.setState ({viewPage:1})
+  }
+
+  ChangeTable = () => {
+    this.setState ({viewPage:2})
+  }
+
   render() {
      let booksCode=this.props.books.map (i=>     
       <Book 
+      viewPage={this.state.viewPage}
       key={i.code}
       title={i.title}
       writer={i.writer}
@@ -24,26 +37,47 @@ class Store extends React.PureComponent {
       
       )
 
-    return (
+
+      if (this.state.viewPage==2){
+      return (
       <div className="Store">
         <div className="Top"><div>{this.props.nameStore}</div></div>
-       <div>
-          
-            {booksCode}
-            
+        <input type="button" value="Список" onClick={this.ChangeList}></input>
+        <input type="button" value="Таблица" onClick={this.ChangeTable}></input>
+        <div>          
+            {booksCode}            
         </div>
         
-      </div>
-    )
-    ;
+       </div> )
+        }
 
-  }
+      if (this.state.viewPage==1){
+      return (
+      <div className="Store">
+        <div className="Top"><div>{this.props.nameStore}</div></div>
+        <input type="button" value="Список" onClick={this.ChangeList}></input>
+        <input type="button" value="Таблица" onClick={this.ChangeTable}></input>
+        <table>     
+             <tbody>
+             <tr className="BookTitle">
+        <td>Название</td>
+        <td>Автор</td>
+        <td>Издательство/Серия</td>
+        <td>Стоимость</td>
+        <td>Наличие</td>
+        </tr>
+               {booksCode} 
+              </tbody>                        
+        </table>
+        </div>
+        )
+        }
 
 }
+}
+
+
 
 export default Store;
-/*       <table>
-          <tbody>
-            {booksCode}
-          </tbody>        
-        </table>*/
+      
+        
